d62e0679081cfdad11f437af28dfa4862fb59d5c
Razeen-Shaikh <razeen9796@outlook.com>
2025-01-21 15:57:31 +0530

    diagonal difference

---
+++ interview-preparation-kits/1-week-preparation-kit/day-1/plus-minus/diagonal-difference.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr) {
+    // Write your code here
+    let left_to_right_sum = 0;
+    let right_to_left_sum = 0;
+    for (let i=0; i<arr.length; i++) {
+        left_to_right_sum += arr[i][i]
+    }
+    
+    for (let i=0; i<arr.length; i++) {
+        right_to_left_sum += arr[i][arr.length-i-1];
+    }
+    
+    return Math.abs(left_to_right_sum - right_to_left_sum);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
