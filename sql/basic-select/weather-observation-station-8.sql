5e092a8add63277829b89f9fe6325ddc5d14c564
Razeen Shaikh <razeen9796@outlook.com>
2025-08-05 17:02:37 +0000

    Resolved Comments

--- sql/basic-select/weather-observation-station-8.sql
+++ sql/basic-select/weather-observation-station-8.sql
@@ -1,1 +1,7 @@
-select distinct(city) from station where city REGEXP '^[aeiou]' and city REGEXP '[aeiou]$';
+select distinct
+    (city)
+from
+    station
+where
+    city REGEXP '^[aeiouAEIOU]'
+    and city REGEXP '[aeiouAEIOU]$';
