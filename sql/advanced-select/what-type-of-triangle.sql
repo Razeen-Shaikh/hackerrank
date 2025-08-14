f151f9fd329e2aa9c6621f3d9c6ce4fbfb0e0320
Razeen Shaikh <razeen9796@outlook.com>
2025-08-14 10:33:18 +0530

    Solved What Type of Triangle

---
+++ sql/advanced-select/what-type-of-triangle.sql
@@ -0,0 +1,14 @@
+SELECT
+    CASE
+        WHEN A + B <= C
+        OR B + C <= A
+        OR A + C <= B THEN 'Not A Triangle'
+        WHEN A = B
+        AND B = C THEN 'Equilateral'
+        WHEN A = B
+        OR B = C
+        OR A = C THEN 'Isosceles'
+        ELSE 'Scalene'
+    END AS triangle_type
+FROM
+    TRIANGLES;
